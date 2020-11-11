ca3878db7e7a5b4de6733c00c73e0b0f7f121618
Mrinal Kumar <kmrinal19@gmail.com>
2020-11-11 20:27:46 +0530

    Install and set-up boilerplate code for redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import Test from './components/test'
+import Test from './test'
 
 function App() {
   return (
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+
+import { combineReducers } from 'redux'
+import exampleReducer from './exampleReducer'
+
+const rootReducers = combineReducers({
+  exampleReducer
+})
+
+export default rootReducers
